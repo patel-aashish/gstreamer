ead8e01df5f4f81ffb9dce090d148c9bad6709c4
Aashish Patel <aashish.a.patel@gmail.com>
2020-08-14 11:37:17 +0530

    Add Plugin Writer Guide temaplte code

---
+++ PluginWritersGuide/gstmyfilter/src/gstmyfilter.h
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2020 Aashish Patel (aashish.a.pate@gmail.com)
+ * 
+ * MyFilter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MyFilter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MyFilter. If not, see <http://www.gnu.org/licenses/>.
+ */
+
