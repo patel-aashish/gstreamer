875bf8ed87d0eda33a5d2ba51590d4b4eb7ba3b3
Aashish Patel <aashish.a.patel@gmail.com>
2020-08-28 12:38:09 +0530

    plugin: add my filter plugin

--- PluginWritersGuide/gstmyfilter/src/gstmyfilter.c
+++ PluginWritersGuide/gstmyfilter/src/gstmyfilter.c
@@ -15,3 +15,75 @@
  * along with MyFilter. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <gst/gst.h>
+
+#include "gstmyfilter.h"
+
+GST_DEBUG_CATEGORY_STATIC (gst_my_filter_debug)
+#define GST_CAT_DEFAULT gst_my_filter_debug
+
+/* Filter signals and arguments */
+enum
+{
+  LAST_SIGNAL
+};
+
+enum
+{
+  PROP_0,
+  PROP_SILENT,
+  PROP_LAST
+};
+
+/* the pad caps */
+static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE ("sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY")
+    );
+
+static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE ("src",
+    GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY")
+    );
+
+#define gst_my_filter_parent_class parent_class
+G_DEFINE_TYPE (GstMyFilter, gst_my_filter, GST_TYPE_ELEMENT);
+
+static void
+gst_my_filter_set_property (GObject *object, guint prop_id,
+    const GValue *value, GParamSpec *pspec);
+static void
+gst_my_filter_get_property (GObject *object, guint prop_id,
+    const GValue *value, GParamSpec *pspec);
+/* GObject vmethod implementations */
+
+/* initialize the myfilter's class */
+static void
+gst_my_filter_class_init (GstMyFilterClass *klass)
+{
+  GObjectClass *gobject_class;
+  GstElementClass *gstelement_class;
+
+  gobject_class = (GstObjectClass *) klass;
+  gstelement_class = (GstElementClass *) klass;
+
+  gobject_class->set_property = gst_my_filter_set_property;
+  gobject_class->get_property = gst_my_filter_get_property;
+
+  g_object_class_install_property (gobject_class, PROP_SILENT,
+      g_param_spec_boolean ("silent", "Silent","Produce verbose output",
+          FALSE, G_PARAM_READWRITE));
+
+  gst_element_class_set_details_simple (gstelement_class,
+      "MyFilter",
+      "Generic",
+      "This filter prints the buffer information",
+      "Aashish Patel <aashish.a.patel@gmail.com>")
+  
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&src_factory));
+  gst_element_class_add_pad_template (gstelement_class,
+      gst_static_pad_template_get (&sink_factory));
+}
--- PluginWritersGuide/gstmyfilter/src/gstmyfilter.h
+++ PluginWritersGuide/gstmyfilter/src/gstmyfilter.h
@@ -15,3 +15,23 @@
  * along with MyFilter. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef _GST_MYFILTER_H_
+#define _GST_MYFILTER_H_
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_MYFILTER (gst_my_filter_get_type())
+G_DECLARE_FINAL_TYPE (GstMyFilter, gst_my_filter, GST, MYFILTER, GstElement)
+
+struct _GstMyFilter
+{
+  GstElement element;
+  GstPad *sinkpad;
+  GstPad *srcpad;
+  gboolean silent;
+};
+
+G_END_DECLS
+#endif // !_GST_MYFILTER_H_
